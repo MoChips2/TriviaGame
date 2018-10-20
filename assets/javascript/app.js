5e8ea1f738cef0ccfdc351a7d71d7be3b362a0da
MoChips <31714669+smedhan1@users.noreply.github.com>
2018-10-20 14:54:51 -0500

    1st update, adding images, styling page

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    //ready to play!
+    //set up questions array
+    /* for each index we need:
+        a question
+        an answer
+        a timer
+        an image? */
+
+    var questions = [
+        {
+            question: "Who was the first team to win the NBA Finals in 1950?",
+            choices: ["Minneapolis Lakers", "Syracuse Nationals", "Fort Wayne Pistons", "New York Knicks"],
+            answer: "Minneapolis Lakers",
+            image: "<img src='../images/Minneapolis_lakers_logo.png' />"
+        }
+
+        {
+            question: "Which team has won the most NBA Finals championships?",
+            choices: ["Chicago Bulls", "Los Angeles Lakers", "Boston Celtics", "Golden State Warriors"],
+            answer: "Boston Celtics",
+            image: "<img src='../images/BOS.logo.svg' />"
+        }
+
+    ];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
